3f142f048d731076e1a1e5bb7a9fd4286a84de1c
Kamalesh Pal <kamalesh.pal.dev@gmail.com>
2026-07-02 23:49:41 +0530

    feat: add countdown timmer using while loop

---
+++ week-01-basics/Countdown.py
@@ -0,0 +1,12 @@
+# Program: Countdown Timmer
+# Concept: while loop
+# week: 1 - python basics
+
+# asking number for timmer
+tim= int(input("Enter a number for countdown: "))
+
+# countdown from number to 0
+
+while tim >= 0:
+    print(tim)
+    tim-=1
