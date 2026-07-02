833cbbbebffb9dfa6e4bd99c7b8112eadd6b76be
Kamalesh Pal <kamalesh.pal.dev@gmail.com>
2026-07-02 21:59:11 +0530

    feat: add temperature converter using type conversion and f-strings

---
+++ week-01-basics/Temp_conv.py
@@ -0,0 +1,13 @@
+# Program: Temperature Converter
+# Concept: variable, input, output, if-else, arithmetic operations, f-strings
+# week: 1 - python basics
+
+#taking input from user
+celsius = float(input("Enter temperature in celsius: ")) 
+
+#coverting celsius into fahrenheit
+fahrenhiet = (celsius*9/5) + 32
+
+#printing result 
+print(f"{celsius} degree celsius is equal to {fahrenhiet} degree fahrenhiet.")
+
