b0c11a94de3c7081b285d72f2ba0c93f6e2e824a
Kamalesh Pal <kamalesh.pal.dev@gmail.com>
2026-07-02 22:50:28 +0530

    feat: add odd even checker with if/elif

---
+++ week-01-basics/odd_even_checker.py
@@ -0,0 +1,12 @@
+# Program: Odd Even checker
+# Concept: if-elif ' modulo operator
+# week: 1 - python basics
+
+# Taking inputs from user
+num = int(input("Enter number: "))
+
+#checking whether number is odd or even
+if num%2 == 0:
+    print("Number is even")
+elif num%2 != 0:
+    print("Number is odd")
